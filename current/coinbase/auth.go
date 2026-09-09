286a28e459a2a46b33810d0bf77ffd0cc76f001f
mzwa <zithendetrading@hotmail.comm>
2026-09-09 12:41:34 +0200

    Use documented Coinbase App JWT issuer

--- current/coinbase/auth.go
+++ current/coinbase/auth.go
@@ -85,7 +85,7 @@ func BuildRESTJWT(credentials Credentials, method, requestHost, requestPath stri
 		URI     string `json:"uri"`
 		*jwt.Claims
 	}{
-		Issuer:  "coinbase-cloud",
+		Issuer:  "cdp",
 		Subject: credentials.KeyID,
 		URI:     fmt.Sprintf("%s %s%s", strings.ToUpper(method), requestHost, requestPath),
 		Claims: &jwt.Claims{
